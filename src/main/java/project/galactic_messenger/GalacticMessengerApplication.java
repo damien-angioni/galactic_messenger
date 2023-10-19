9a195705a65e56a1d0e6a4a1c70961fb58e47019
damien-angioni <115216654+damien-angioni@users.noreply.github.com>
2023-10-19 11:46:47 +0200

    Project setup change
    -More accurate Metadata.
    -More accurate/usefull dependencies.

---
+++ src/main/java/project/galactic_messenger/GalacticMessengerApplication.java
@@ -0,0 +1,13 @@
+package project.galactic_messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticMessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/galactic_messenger/GalacticMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.galactic_messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalacticMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
